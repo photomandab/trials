91d67ac8e9676082cd7fab69e32e3699b8b72096
photomandab <davidbigwood@gmail.com>
2016-06-02 13:20:57 -0400

    Refactored to simplify ResultPayload

--- src/com/logicnow/comparison/CompUtils.java
+++ src/com/logicnow/comparison/CompUtils.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -33,6 +34,7 @@ import org.apache.poi.ss.usermodel.Row;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import com.logicnow.comparison.ReportComparator.CombinedRow;
 import com.opencsv.CSVWriter;
 
 public class CompUtils {
@@ -176,6 +178,15 @@ public class CompUtils {
 		return Arrays.asList(vals);
 	}
 
+	@SafeVarargs
+	public static <T> Set<T> newSet(List<T> ... vals) {
+		HashSet<T> set = Sets.newHashSet();
+		for (List<T> l : vals) {
+			set.addAll(l);
+		}
+		return set;
+	}
+
 	public static String writeSqlWhereClause(String product, List<Set<String>> list) {
 		StringBuilder buffy = new StringBuilder();
 		Set<String> uniqueLeads = Sets.newHashSet();
@@ -208,13 +219,13 @@ public class CompUtils {
 		for (ResultPayload p : payloads) {
 			String product = p.getProduct();
 			HSSFSheet sheet = wb.createSheet(product + " Info");
-			p.populateInfoSheet(sheet);
+			CompUtils.populateInfoSheet(p, sheet);
 		}
 		// Add product sheets
 		for (ResultPayload p : payloads) {
 			String product = p.getProduct();
 			HSSFSheet sheet = wb.createSheet(product);
-			p.populateSheet(sheet);
+			CompUtils.populateSheet(p, sheet);
 		}
 
 		// Add Amarillo All sheet
@@ -405,5 +416,316 @@ public class CompUtils {
 		return region;
 	}
 
+	public static List<CombinedRow> getCombinedRecords(ResultPayload payload) {
+		List<CombinedRow> records = Lists.newArrayList();
+		TreeSet<String> allSorted = getSortedSet(payload.getCombined());
+		CombinedRow row = null;
+		String product = payload.getProduct();
+		for (String tenant : allSorted) {
+			// We don't need to see items that are both/neither valid
+			if (payload.getBothValid().contains(tenant)) continue;
+			if (payload.getNeitherValid().contains(tenant)) continue;
+			// Generate the remaining entries
+			row = new CombinedRow(tenant);
+			row.leadId = CompUtils.generateAmarilloId(tenant, product);
+			if (payload.getBoth().contains(tenant)) row.inBoth = 1;
+			if (payload.getAmarilloOnly().contains(tenant)) row.inAmarillo = 1;
+			if (payload.getSfdcOnly().contains(tenant)) row.inSFDC = 1;
+			if (payload.getAmarilloValid().contains(tenant)) row.validInAmarillo = 1;
+			if (payload.getSfdcValid().contains(tenant)) row.validInSFDC = 1;
+			if (payload.getMismatchValidity().contains(tenant)) row.mismatch = 1;
+			if (payload.getSfdcDupes().contains(tenant)) row.dupeInSFDC = 1;
+			// Try and establish a reason for discrepancy
+			Pair<String, String> reasons = establishReason(payload, row);
+			row.reason1 = reasons != null ? reasons.getLeft() : "";
+			row.reason2 = reasons != null ? reasons.getRight() : "";
+			// Add the item to the list
+			records.add(row);
+		}
+		return records;
+	}
+
+	public static void populateSheet(ResultPayload payload, HSSFSheet sheet) {
+		List<CombinedRow> records = getCombinedRecords(payload);
+		int rownum = 0;
+		Row row = sheet.createRow(rownum++);
+		int cellnum = 0;
+		// Headers
+		for (String value : CompUtils.COMBINED_HEADERS) {
+			Cell cell = row.createCell(cellnum++);
+			cell.setCellValue(value);
+		}
+		// Data
+		for (CombinedRow item : records) {
+			row = sheet.createRow(rownum++);
+			cellnum = 0;
+			Cell cell = null;
+			for (Object obj : item.getData()) {
+				cell = row.createCell(cellnum++);
+				if(obj instanceof Date) {
+					cell.setCellValue((Date)obj);
+				} else if(obj instanceof Boolean) {
+					cell.setCellValue((Boolean)obj);
+				} else if(obj instanceof String) {
+					cell.setCellValue((String)obj);
+				} else if(obj instanceof Double) {
+					cell.setCellValue((Double)obj);
+				} else if(obj instanceof Integer) {
+					cell.setCellValue((Integer)obj);
+				}
+			}
+		}
+		for (int i = 0; i < cellnum; i++) {
+			CompUtils.autoSizeColumn(sheet, i);
+		}
+	}
+
+	private static Pair<String, String> establishReason(ResultPayload payload, CombinedRow item) {
+		String tenantId = item.tenantId;
+		List<CSVRecord> aRecords = payload.getAmarilloAllMap().get(tenantId);
+		List<CSVRecord> sRecords = payload.getSfdcAllMap().get(tenantId);
+		List<CSVRecord> fRecords = payload.getFeedMap().get(tenantId);
+		
+		// Ensure we have records to make a comparison
+		if (CompUtils.isEmpty(aRecords) && (CompUtils.isEmpty(sRecords) && CompUtils.isEmpty(fRecords)))
+			return null;
+
+		Pair<String, String> reason = null;
+		if ((reason = checkTerritoryChange(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		} else if ((reason = checkProductChange(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		} else if ((reason = checkReUsedTenant(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		} else if ((reason = checkEmployeeTesting(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		} else if ((reason = checkSFDCDupesVaryingValidity(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		} else if ((reason = checkTimingIssue(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		} else if ((reason = checkCustomerTrial(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		} else if ((reason = checkOpportunityType(payload, item, aRecords, sRecords, fRecords)) != null) {
+			return reason;
+		}
+		
+		return null;
+	}
+
+	private static Pair<String, String> checkOpportunityType(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		List<String> attributes = getAttributes(payload.getFeedRecords().getLeft(), fRecords, "OpportunityType");
+		String value = CompUtils.getListValueOrMultiple(attributes);
+		if (value != null) {
+			String val = value.toLowerCase();
+			if (val.contains("renewal") || val.contains("migration") || val.contains("customer")) {
+				return Pair.of("Excluded from SFDC", "Type " + value);					
+			} 
+		}
+		return null;
+	}
+
+	private static Pair<String, String> checkCustomerTrial(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		List<String> attributes = getAttributes(payload.getFeedRecords().getLeft(), fRecords, "Source");
+		String value = CompUtils.getListValueOrMultiple(attributes);
+		if ("Customer Trial".equals(value)) {
+			return Pair.of("Excluded from SFDC", "Source is Customer Trial");					
+		}
+		return null;
+	}
+
+	private static Pair<String, String> checkTimingIssue(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		Date start = payload.getStartDate();
+		Date end = payload.getEndDate();
+		List<String> attributes = getAttributes(payload.getFeedRecords().getLeft(), fRecords, "Trial_Start");
+		String value = CompUtils.getListValueOrMultiple(attributes);
+		Date trialStart = (value != null && !CompUtils.MULTIPLE.equals(value)) ? parseDate(value) : null;
+		if (trialStart != null) {
+			if (trialStart.before(start) || trialStart.after(end)) {
+				return Pair.of("Timing Issue", "Trial Start out of Range in SFDC");					
+			}
+		}
+		return null;
+	}
+
+	private static Pair<String, String> checkSFDCDupesVaryingValidity(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		if (payload.getSfdcDupes().contains(item.tenantId)) {
+			List<String> attributes = getAttributes(payload.getSfdcAllRecords().getLeft(), sRecords, "Is Valid");
+			String value = CompUtils.getListValueOrMultiple(attributes);
+			if (CompUtils.MULTIPLE.equals(value)) {
+				return Pair.of("Multiple entry in SFDC", "Different validity values");
+			}
+		}
+		return null;
+	}
+
+	private static Pair<String, String> checkEmployeeTesting(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		List<String> attribution = getAttributes(payload.getAmarilloAllRecords().getLeft(), aRecords, "LN Attribution Group");
+		String value = CompUtils.getListValueOrMultiple(attribution);
+		if ("Employee Testing".equals(value)) {
+			return Pair.of("Employee Testing", "Amarillo identifies as Test Trial");
+		}
+		return null;
+	}
+
+	private static Pair<String, String> checkReUsedTenant(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		if (item.isInAmarilloOnly()) {
+			List<String> tenants = getAttributes(payload.getFeedRecords().getLeft(), fRecords, "TenantID");
+			if (CompUtils.isEmpty(tenants)) {
+				return Pair.of("Not in SFDC Feed", "Validity not read from SFDC");
+			}
+		}
+		return null;
+	}
+
+	private static Pair<String, String> checkProductChange(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		List<String> aProducts = getAttributes(payload.getAmarilloAllRecords().getLeft(), aRecords, "Fixed Product");
+		List<String> sProducts = getAttributes(payload.getSfdcAllRecords().getLeft(), sRecords, "Core Product");
+		List<String> fProducts = getAttributes(payload.getFeedRecords().getLeft(), fRecords, "Product");
+		
+		String aProduct = !CompUtils.isEmpty(aProducts) ? CompUtils.getListValueOrMultiple(aProducts) : null;		
+		String sProduct = !CompUtils.isEmpty(sProducts) ? CompUtils.getListValueOrMultiple(sProducts) : null;
+		String fProduct = !CompUtils.isEmpty(fProducts) ? CompUtils.getListValueOrMultiple(fProducts) : null;
+		
+		aProduct = CompUtils.mappedProduct(aProduct);
+		sProduct = CompUtils.mappedProduct(sProduct);
+		fProduct = CompUtils.mappedProduct(fProduct);
+		
+		if (item.isInBoth() && !CompUtils.isBlank(aProduct) && !aProduct.equals(sProduct)) {
+			return Pair.of("Product Change", "1:" + aProduct + " in Amarillo, " + sProduct + " in SFDC");
+		} else if (item.isInAmarilloOnly() && !CompUtils.isBlank(sProduct) && !CompUtils.isBlank(aProduct) && !aProduct.equals(sProduct)) {
+			return Pair.of("Product Change", "2:" + aProduct + " in Amarillo, " + sProduct + " in SFDC");
+		} else if (item.isInAmarilloOnly() && !CompUtils.isBlank(fProduct) && !CompUtils.isBlank(aProduct) && !aProduct.equals(fProduct)) {
+			return Pair.of("Product Change", "3:" + aProduct + " in Amarillo, " + fProduct + " in SFDC Feed");
+		} else if (item.isInSFDCOnly() && !CompUtils.isBlank(sProduct) && !CompUtils.isBlank(aProduct) && !sProduct.equals(aProduct)) {
+			return Pair.of("Product Change", "4:" + aProduct + " in Amarillo, " + sProduct + " in SFDC");
+		}
+		
+		return null;
+	}
+
+	private static Pair<String, String> checkTerritoryChange(ResultPayload payload, CombinedRow item, List<CSVRecord> aRecords, List<CSVRecord> sRecords, List<CSVRecord> fRecords) {
+		List<String> aRegions = getAttributes(payload.getAmarilloAllRecords().getLeft(), aRecords, "Marketing Territory");
+		List<String> sRegions = getAttributes(payload.getSfdcAllRecords().getLeft(), sRecords, "Group");
+		List<String> fRegions = getAttributes(payload.getFeedRecords().getLeft(), fRecords, "Sub-Region");
+		
+		String aRegion = aRegions != null && aRegions.size() > 0 ? CompUtils.getListValueOrMultiple(aRegions) : null;		
+		String sRegion = sRegions != null && sRegions.size() > 0 ? CompUtils.getListValueOrMultiple(sRegions) : null;
+		String fRegion = fRegions != null && fRegions.size() > 0 ? CompUtils.getListValueOrMultiple(fRegions) : null;
+		
+		aRegion = CompUtils.mappedRegion(aRegion);
+		sRegion = CompUtils.mappedRegion(sRegion);
+		fRegion = CompUtils.mappedRegion(fRegion);
+		
+		if (item.isInBoth() && sRegion != null && !CompUtils.isBlank(aRegion) && !aRegion.equals(sRegion)) {
+			return Pair.of("Territory Change", "1:" + aRegion + " in Amarillo, " + sRegion + " in SFDC");
+		} else if (item.isInAmarilloOnly() && sRegion != null && aRegion != null && !aRegion.equals(sRegion)) {
+			return Pair.of("Territory Change", "2:" + aRegion + " in Amarillo, " + sRegion + " in SFDC");
+		} else if (item.isInAmarilloOnly() && fRegion != null && aRegion != null && !aRegion.equals(fRegion)) {
+			return Pair.of("Territory Change", "3:" + aRegion + " in Amarillo, " + fRegion + " in SFDC Feed");
+		} else if (item.isInSFDCOnly() && sRegion != null && aRegion != null && !aRegion.equals(sRegion)) {
+			return Pair.of("Territory Change", "4:" + aRegion + " in Amarillo, " + sRegion + " in SFDC");
+		}
+		
+		return null;
+	}
+
+	private static List<String> getAttributes(String[] headers, List<CSVRecord> records, String attributeName) {
+		List<String> values = Lists.newArrayList();
+		if (records != null) {
+			for (CSVRecord record : records) {
+				String value = record.toMap(headers).get(attributeName);
+				if (value != null) values.add(value);
+			}
+		}
+		return values;
+	}
+
+	public static void populateInfoSheet(ResultPayload payload, HSSFSheet sheet) {
+		// Totals
+		int bothSize = payload.getBoth().size();
+		int onlyAmarilloSize = payload.getAmarilloOnly().size();
+		int onlySFDCSize = payload.getSfdcOnly().size();
+		int totalSize = bothSize + onlyAmarilloSize + onlySFDCSize;
+		int dupesSize = payload.getSfdcDupes().size();
+
+		int bothValidSize = payload.getBothValid().size();
+		int neitherValidSize = payload.getNeitherValid().size();
+		int onlyAmarilloValidSize = payload.getAmarilloValid().size();
+		int onlySFDCValidSize = payload.getSfdcValid().size();
+		int mismatchSize = payload.getMismatchValidity().size();
+		int totalValidLeads = bothValidSize + neitherValidSize + onlyAmarilloValidSize + onlySFDCValidSize;
+		int diffSize = totalSize - bothValidSize - neitherValidSize;
+		
+		int rownum = 0;
+		String product = payload.getProduct();
+		
+		// Overview
+		rownum = addHeaderRow(sheet, rownum, product + " Trial Count");
+		rownum = addDataRow(sheet, rownum, "Total", new Object[] { totalSize, "100%" });
+		rownum = addDataRow(sheet, rownum, "In Both", new Object[] { bothSize, CompUtils.toPercentage(bothSize, totalSize) });
+		rownum = addDataRow(sheet, rownum, "In Amarillo Only", new Object[] { onlyAmarilloSize, CompUtils.toPercentage(onlyAmarilloSize, totalSize) });
+		rownum = addDataRow(sheet, rownum, "In SFDC Only", new Object[] { onlySFDCSize, CompUtils.toPercentage(onlySFDCSize, totalSize) });
+		rownum = addDataRow(sheet, rownum, "Dupes in SFDC", new Object[] { dupesSize, CompUtils.toPercentage(dupesSize, totalSize) });
+		rownum = addBlankRow(sheet, rownum);
+		rownum = addHeaderRow(sheet, rownum, product + " Trial Validity");
+		rownum = addDataRow(sheet, rownum, "Total", new Object[] { totalSize, "100%" });
+		rownum = addDataRow(sheet, rownum, "Total Valid", new Object[] { totalValidLeads, CompUtils.toPercentage(totalValidLeads, totalSize) });
+		rownum = addDataRow(sheet, rownum, "Both", new Object[] { bothValidSize, CompUtils.toPercentage(bothValidSize, totalSize) });
+		rownum = addDataRow(sheet, rownum, "Neither", new Object[] { neitherValidSize,CompUtils.toPercentage(neitherValidSize, totalSize) });
+		rownum = addDataRow(sheet, rownum, "Amarillo Only", new Object[] { onlyAmarilloValidSize, CompUtils.toPercentage(onlyAmarilloValidSize, totalSize), CompUtils.getDBString(payload.getAmarilloValid())});
+		rownum = addDataRow(sheet, rownum, "SFDC Only", new Object[] { onlySFDCValidSize, CompUtils.toPercentage(onlySFDCValidSize, totalSize), CompUtils.getDBString(payload.getSfdcValid()) });
+		rownum = addDataRow(sheet, rownum, "Mismatch", new Object[] { mismatchSize, CompUtils.toPercentage(mismatchSize, totalSize), CompUtils.getDBString(payload.getMismatchValidity()) });
+		rownum = addDataRow(sheet, rownum, "Differences", new Object[] { diffSize, CompUtils.toPercentage(diffSize, totalSize) });
+		rownum = addBlankRow(sheet, rownum);
+		rownum = addDataRow(sheet, rownum, "SQL", new Object[] { payload.getSqlWhereClause() });
+		
+	}
+
+	private static int addDataRow(HSSFSheet sheet, int rownum, String msg, Object[] data) {
+		Row row = sheet.createRow(rownum++);
+		int cellnum = 0;		
+		Cell cell = row.createCell(cellnum++);
+		cell.setCellValue(msg);
+		for (Object obj : data) {
+			cell = row.createCell(cellnum++);
+			if(obj instanceof Date) {
+				cell.setCellValue((Date)obj);
+			} else if(obj instanceof Boolean) {
+				cell.setCellValue((Boolean)obj);
+			} else if(obj instanceof String) {
+				cell.setCellValue((String)obj);
+			} else if(obj instanceof Double) {
+				cell.setCellValue((Double)obj);
+			} else if(obj instanceof Integer) {
+				cell.setCellValue((Integer)obj);
+			}
+		}
+		return rownum;
+	}
+
+	private static int addBlankRow(HSSFSheet sheet, int rownum) {
+		Row row = sheet.createRow(rownum++);
+		int cellnum = 0;		
+		Cell cell = row.createCell(cellnum++);
+		cell.setCellValue("");
+		return rownum;
+	}
+
+	private static int addHeaderRow(HSSFSheet sheet, int rownum, String msg) {
+		Row row = sheet.createRow(rownum++);
+		int cellnum = 0;		
+		Cell cell = row.createCell(cellnum++);
+		cell.setCellValue(msg);
+		rownum = addBlankRow(sheet, rownum);
+		return rownum;
+	}
+
+	public void addFilteredAmarilloData(HSSFWorkbook wb, HSSFSheet sheet, ResultPayload payload) {
+		CompUtils.addRecordsToSheet(wb, sheet, payload.getAmarilloRecords(), payload.getConfigs().getLeft());
+	}
+
+	public void addFilteredSFDCData(HSSFWorkbook wb, HSSFSheet sheet, ResultPayload payload) {		
+		CompUtils.addRecordsToSheet(wb, sheet, payload.getSfdcRecords(), payload.getConfigs().getRight());
+	}
 
 }
